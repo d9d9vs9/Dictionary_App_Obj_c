34138324c738307b94683f33ec3c953600e760b8
Dmitry Chumakov <dima.chumakovwork@gmail.com>
2020-12-15 17:59:23 +0200

    added ResultSaveCompletedBlock

---
+++ MyDictionary/MyDictionary/Utilites/CompletedBlocks/ResultSaveCompletedBlock.h
@@ -0,0 +1,10 @@
+//
+//  ResultSaveCompletedBlock.h
+//  MyDictionary
+//
+//  Created by Дмитрий Чумаков on 15.12.2020.
+//
+
+#import "Result.h"
+
+typedef void (^ResultSaveCompletedBlock) (Result result, NSError * _Nullable error);
